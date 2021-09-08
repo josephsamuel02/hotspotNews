6f2105fa645519fcd3ee2edb0dcb59f227b983e6
josephsamuel02 <mrsamd02@gmail.com>
2021-09-08 18:39:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const sublist = document.querySelector('#sublist');
+
+const body = document.querySelector('body');
+
+const discover = document.querySelector('#discover');
+
+
+discover.addEventListener( 'mouseover', () => {
+sublist.style.display='contents';
+});
+
+// discover.addEventListener( 'mouseleave', () => {
+// sublist.style.display='contents';
+// });
+    
